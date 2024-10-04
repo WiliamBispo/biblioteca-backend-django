6946af57a931570fada0164ab494a90a8ff70cc3
Wiliam Bispo <wiliam_bis14@hotmail.com>
2024-10-04 08:36:01 -0300

    update nas views and urls

--- biblioteca/core/urls.py
+++ biblioteca/core/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('livros/', views.livro_list_create, name='livros-list-create'),
-    path('livros/<int:pk>/', views.livro_detail, name='livro-detail'),
+
+    path('livros/', views.LivroList.as_view(), name=views.LivroList.name),
+    path('livros/<int:pk>/', views.LivroDetail.as_view(),
+         name=views.LivroDetail.name),
+
+    path('categoria/', views.CategoriaList.as_view(),
+         name=views.CategoriaList.name),
+    path('categoria/<int:pk>/', views.CategoriaDetail.as_view(),
+         name=views.CategoriaDetail.name),
+
+    path('autor/', views.AutorList.as_view(), name=views.AutorList.name),
+    path('autor/<int:pk>/', views.AutorDetail.as_view(),
+         name=views.AutorDetail.name),
+
 ]
--- biblioteca/core/views.py
+++ biblioteca/core/views.py
@@ -1,55 +1,43 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
-from rest_framework import status
+from rest_framework import generics
 from .models import Livro
+from .models import Categoria
+from .models import Autor
 from .serializers import LivroSerializer
+from .serializers import CategoriaSerializer
+from .serializers import AutorSerializer
 
 
-class JSONResponse(HttpResponse):
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
+class LivroList(generics.ListCreateAPIView):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+    name = "livro-list"
 
 
-@csrf_exempt
-def livro_list_create(request):
-    if request.method == 'GET':
-        livros = Livro.objects.all()
-        serializer = LivroSerializer(livros, many=True)
-        return JSONResponse(serializer.data)
-
-    elif request.method == 'POST':
-        livro_data = JSONParser().parse(request)
-        serializer = LivroSerializer(data=livro_data)
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data, status=status.HTTP_201_CREATED)
-        return JSONResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class LivroDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+    name = "livro-detail"
 
 
-@csrf_exempt
-def livro_detail(request, pk):
-    try:
-        livro = Livro.objects.get(pk=pk)
-    except Livro.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+class CategoriaList(generics.ListCreateAPIView):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+    name = "categoria-list"
 
-    if request.method == 'GET':
-        serializer = LivroSerializer(livro)
-        return JSONResponse(serializer.data)
 
-    elif request.method == 'PUT':
-        livro_data = JSONParser().parse(request)
-        serializer = LivroSerializer(livro, data=livro_data)
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class CategoriaDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+    name = "categoria-detail"
 
-    elif request.method == 'DELETE':
-        livro.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+
+class AutorList(generics.ListCreateAPIView):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+    name = "autor-list"
+
+
+class AutorDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+    name = "autor-detail"
